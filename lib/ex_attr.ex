bf7e439dc86b64638069aef67ec5a6f8da05ffe6
Ethan Larkham <ethanlarkham@protonmail.com>
2024-06-11 16:43:31 -0400

    Align error semantics with Elixir/Erlang file operations

--- lib/ex_attr.ex
+++ lib/ex_attr.ex
@@ -14,16 +14,21 @@ defmodule ExAttr do
   I'm picky about how I handle serialization and was looking for something less opinionated
   and "dumb" that I could easily wrap my application specific logic around.
 
-  Since I couldn't find anything that fit this criteria I figured I'd just do it myself, and so here we are!
+  Since I couldn't find anything that fit this criteria I figured I'd just do it myself,
+  and so here we are!
 
   ### Namespacing Advise
 
-  If you want to add some custom-application-specific xattrs to a file, it's generally a good idea to do this all
-  under a common namespace specific to your application. Since POSIX extended attributes already has some built-in
-  namespacing that differentiates between `trusted`, `system`, `user`, etc, it is highly likely you will need to make
-  your namespace a child of the `user` namespace (ex: `"user.<my_namespace>"`) unless you are doing SELinux stuff.
+  If you want to add some custom-application-specific xattrs to a file, it's generally a
+  good idea to do this all under a common namespace specific to your application. Since
+  POSIX extended attributes already has some built-in namespacing that differentiates
+  between `trusted`, `system`, `user`, etc, it is highly likely you will need to make
+  your namespace a child of the `user` namespace (ex: `"user.<my_namespace>"`)
+  unless you are doing SELinux stuff.
 
-  This is most easily abstracted away by simply wrapping the functions of this library to auto handle all that for you.
+  This is most easily abstracted away by simply wrapping the functions of this library
+  to auto handle all that for you.
+
   Here is an example of what I mean:
   ```elixir
   defmodule MyApp.XAttr do
@@ -57,6 +62,32 @@ defmodule ExAttr do
     # And so on...
   end
   ```
+
+  ### Error Handling
+  When possible, POSIX errors are normalized as atoms to be compatible with the error
+  semantics of the `File` & `:file` modules. If it's either not a POSIX error, or one I
+  never anticipated, then it will fallback to being a generic string error instead.
+
+  Since these align with existing semantics of erlang, you can use `:file.format_error/1` to
+  get the error string for any atoms returned here. Alternativey you can get them directly
+  via `:erl_posix_msg.message/1` which `:file.format_error/1` uses under the hood.
+
+  Below is a table that lists all possible POSIX errors and what could trigger them, the
+  description used might not align with what `:file.format_error/1` returns.
+
+  | Error    | Description |
+  |----------|-------------|
+  | :e2big   | Attribute value is too big |
+  | :eacces  | Permission denied |
+  | :einval  | Invalid argument |
+  | :eio     | Generic I/O Error |
+  | :enodata | Attribute does not exist (aliased with enoattr) |
+  | :enoent  | Does not exist |
+  | :enomem  | Out of memory issue, rare |
+  | :enospc  | No space on device |
+  | :eperm   | Operation not permitted |
+  | :erofs   | Read-only filesystem |
+  | :enotsup | Filesystem or platform not supported |
   """
   alias ExAttr.Nif
 
@@ -79,8 +110,21 @@ defmodule ExAttr do
 
     @impl true
     def message(%{action: action, reason: reason, path: path}) do
-      "could not #{action} #{inspect(path)}: #{reason}"
+      reason = format(reason)
+      "could not #{action} #{inspect(path)} :: #{reason}"
     end
+
+    def format(:e2big) do
+      "e2big :: attribute value is too big"
+    end
+    def format(:enodata) do
+      "enodata :: attribute does not exist"
+    end
+    def format(posix_err) when is_atom(posix_err) do
+      "#{posix_err} :: #{:file.format_error(posix_err)}"
+    end
+    def format(reason) when is_binary(reason), do: reason
+
   end
 
   #############
@@ -90,15 +134,29 @@ defmodule ExAttr do
   @type name()  :: String.t()
   @type value() :: String.t() | nil
 
-  @type result(t) :: {:ok, t} | {:error, String.t()}
-  @type result    :: :ok      | {:error, String.t()}
+  @type result(t) :: {:ok, t} | {:error, error_reason()}
+  @type result    :: :ok      | {:error, error_reason()}
+
+  @type error_reason ::
+      String.t()
+    | :e2big   # Attribute is too big
+    | :eacces  # Permission denied
+    | :einval  # Invalid argument
+    | :eio     # Generic I/O Error
+    | :enodata # Attribute does not exist (aliased with enoattr)
+    | :enoent  # Does not exist
+    | :enomem  # Out of memory issue, rare
+    | :enospc  # No space on device
+    | :eperm   # Operation not permitted
+    | :erofs   # When read-only filesystem
+    | :enotsup # When filesystem or platform not supported
 
   #################
   #   Functions   #
   #################
 
   @doc """
-  Forwards the result of the `xattr:SUPPORTED_PLATFORM` function from
+  Forwards the result of the `xattr:SUPPORTED_PLATFORM` constant from
   the `xattr` crate this library wraps.
 
   Returns true if platform is supported.
@@ -130,6 +188,9 @@ defmodule ExAttr do
       nil ->
         {:ok, nil}
 
+      error when is_atom(error) ->
+        {:error, error}
+
       value ->
         {:ok, to_string(value)}
     end
@@ -143,7 +204,7 @@ defmodule ExAttr do
     case get(path, name) do
       {:error, reason} ->
         raise Error,
-          action: "get xattr #{inspect name}",
+          action: "get xattr #{inspect name} from",
           path: path,
           reason: reason
 
@@ -170,12 +231,21 @@ defmodule ExAttr do
   def set(path, name, nil) do
     case remove(path, name) do
       :ok -> :ok
-      {:error, "No data available (os error 61)"} -> :ok
+      {:error, :enodata} -> :ok
       {:error, reason} -> {:error, reason}
     end
   end
   def set(path, name, value) do
-    Nif.set_xattr(path, name, to_string(value))
+    case Nif.set_xattr(path, name, to_string(value)) do
+      :ok ->
+        :ok
+
+      error when is_atom(error) ->
+        {:error, error}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
   end
 
   @doc """
@@ -186,7 +256,7 @@ defmodule ExAttr do
     case set(path, name, value) do
       {:error, reason} ->
         raise Error,
-          action: "set xattr #{inspect name} -> #{inspect value}",
+          action: "set xattr #{inspect name} -> #{inspect value} for",
           path: path,
           reason: reason
 
@@ -213,7 +283,16 @@ defmodule ExAttr do
   """
   @spec remove(Path.t(), name()) :: result()
   def remove(path, name) do
-    Nif.remove_xattr(path, name)
+    case Nif.remove_xattr(path, name) do
+      :ok ->
+        :ok
+
+      error when is_atom(error) ->
+        {:error, error}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
   end
 
   @doc """
@@ -221,10 +300,10 @@ defmodule ExAttr do
   """
   @spec remove!(Path.t(), name()) :: :ok
   def remove!(path, name) do
-    case Nif.remove_xattr(path, name) do
+    case remove(path, name) do
       {:error, reason} ->
         raise Error,
-          action: "remove xattr #{inspect name}",
+          action: "remove xattr #{inspect name} from",
           path: path,
           reason: reason
 
@@ -250,8 +329,14 @@ defmodule ExAttr do
   @spec list(Path.t()) :: result(list(name()))
   def list(path) do
     case Nif.list_xattr(path) do
-      {:error, reason} -> {:error, reason}
-      value -> {:ok, value}
+      {:error, reason} ->
+        {:error, reason}
+
+      error when is_atom(error) ->
+        {:error, error}
+
+      value ->
+        {:ok, value}
     end
   end
 
@@ -266,7 +351,7 @@ defmodule ExAttr do
     case Nif.list_xattr(path) do
       {:error, reason} ->
         raise Error,
-          action: "list xattr",
+          action: "list xattr for",
           path: path,
           reason: reason
 
@@ -318,7 +403,7 @@ defmodule ExAttr do
 
       {:error, reason} ->
         raise Error,
-          action: "dump xattr",
+          action: "dump xattr for",
           path: path,
           reason: reason
     end
--- native/ex_attr_nif/src/lib.rs
+++ native/ex_attr_nif/src/lib.rs
@@ -1,5 +1,32 @@
 use rustler::{Atom, Error, NifResult, Binary};
 use rustler::types::atom;
+use rustix::io::Errno;
+use std::io;
+
+// Sucess means it will be encoded as an atom (from static string)
+// Failure means it will be encoded as a string 
+fn io_error_to_atom(err: io::Error) -> Result<&'static str, String> {
+    if let Some(code) = err.raw_os_error() {
+        match Errno::from_raw_os_error(code) {
+            Errno::TOOBIG => Ok("e2big"),
+            Errno::ACCESS => Ok("eacces"),
+            Errno::INVAL  => Ok("einval"),
+            Errno::IO     => Ok("eio"),
+            Errno::NODATA => Ok("enodata"),
+            Errno::NOENT  => Ok("enoent"),
+            Errno::NOMEM  => Ok("enomem"),
+            Errno::NOSPC  => Ok("enospc"),
+            Errno::PERM   => Ok("eperm"),
+            Errno::ROFS   => Ok("erofs"),
+            Errno::NOTSUP => Ok("enotsup"),
+            _ => Err(err.to_string()),
+        }
+    } else if err.kind() == io::ErrorKind::Unsupported {
+        Ok("enotsup")
+    } else {
+        Err(err.to_string())
+    }
+}
 
 #[rustler::nif]
 fn supported_platform() -> bool {
@@ -11,7 +38,10 @@ fn get_xattr(path: String, name: String) -> NifResult<Option<Vec<u8>>> {
     match xattr::get(path, name) {
         Ok(Some(value)) => Ok(Some(value)),
         Ok(None) => Ok(None),
-        Err(e) => Err(Error::Term(Box::new(e.to_string())))
+        Err(e) => match io_error_to_atom(e) {
+            Ok(atom_str) => Err(Error::Atom(atom_str)),
+            Err(msg) => Err(Error::Term(Box::new(msg))),
+        },
     }
 }
 
@@ -19,7 +49,10 @@ fn get_xattr(path: String, name: String) -> NifResult<Option<Vec<u8>>> {
 fn set_xattr(path: String, name: String, value: Binary) -> NifResult<Atom> {
     match xattr::set(path, name, value.as_slice()) {
         Ok(_) => Ok(atom::ok()),
-        Err(e) => Err(Error::Term(Box::new(e.to_string())))
+        Err(e) => match io_error_to_atom(e) {
+            Ok(atom_str) => Err(Error::Atom(atom_str)),
+            Err(msg) => Err(Error::Term(Box::new(msg))),
+        },
     }
 }
 
@@ -27,9 +60,12 @@ fn set_xattr(path: String, name: String, value: Binary) -> NifResult<Atom> {
 fn list_xattr(path: String) -> NifResult<Vec<String>> {
     match xattr::list(path) {
         Ok(attrs) => attrs.map(|attr| {
-            attr.into_string().map_err(|_| Error::Term(Box::new("Failed to convert OsString")))
+            attr.into_string().map_err(|_| Error::Term(Box::new("Failed to convert OsString".to_string())))
         }).collect(),
-        Err(e) => Err(Error::Term(Box::new(e.to_string())))
+        Err(e) => match io_error_to_atom(e) {
+            Ok(atom_str) => Err(Error::Atom(atom_str)),
+            Err(msg) => Err(Error::Term(Box::new(msg))),
+        },
     }
 }
 
@@ -37,14 +73,17 @@ fn list_xattr(path: String) -> NifResult<Vec<String>> {
 fn remove_xattr(path: String, name: String) -> NifResult<Atom> {
     match xattr::remove(path, name) {
         Ok(_) => Ok(atom::ok()),
-        Err(e) => Err(Error::Term(Box::new(e.to_string())))
+        Err(e) => match io_error_to_atom(e) {
+            Ok(atom_str) => Err(Error::Atom(atom_str)),
+            Err(msg) => Err(Error::Term(Box::new(msg))),
+        },
     }
 }
 
 rustler::init!("Elixir.ExAttr.Nif", [
     supported_platform,
-    get_xattr, 
-    set_xattr, 
-    list_xattr, 
+    get_xattr,
+    set_xattr,
+    list_xattr,
     remove_xattr,
 ]);
